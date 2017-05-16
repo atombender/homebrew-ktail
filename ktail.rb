fea4656bbfa839653facc37a5d4eb29d94d5cede
Alexander Staubo <alex@purefiction.net>
2017-05-16 14:26:06 -0400

    Add formula.

---
+++ ktail.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Ktail < Formula
+  homepage 'https://github.com/atombender/ktail'
+  url 'https://github.com/atombender/ktail.git',
+    :tag => 'v0.2.0'
+
+  depends_on "go" => :build
+  depends_on "glide" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/atombender/ktail"
+    dir.install buildpath.children
+
+    cd dir do
+      system "glide install -v"
+      system "make"
+      bin.install "build/ktail"
+    end
+  end
+
+  test do
+    system "#{bin}/ktail", "--help"
+  end
+end
