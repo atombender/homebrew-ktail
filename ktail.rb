c7423b8b8c78727d354f2d2513cc9a13cd02d01c
Alexander Staubo <alex@purefiction.net>
2017-05-16 19:47:39 -0400

    0.3.0.

--- ktail.rb
+++ ktail.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ktail < Formula
   homepage 'https://github.com/atombender/ktail'
   url 'https://github.com/atombender/ktail.git',
-    :tag => 'v0.2.0'
+    :tag => 'v0.3.0'
 
   depends_on "go" => :build
   depends_on "glide" => :build
