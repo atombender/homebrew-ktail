017a8c7e5a17f6d4c80417af9fa863bb5840b028
Alexander Staubo <alex@purefiction.net>
2018-06-09 01:33:05 -0400

    v0.9.0

--- ktail.rb
+++ ktail.rb
@@ -3,18 +3,18 @@ require 'formula'
 class Ktail < Formula
   homepage 'https://github.com/atombender/ktail'
   url 'https://github.com/atombender/ktail.git',
-    :tag => 'v0.8.0'
+    :tag => 'v0.9.0'
 
   depends_on "go" => :build
-  depends_on "dep" => :build
+  depends_on "glide" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/atombender/ktail"
+    dir = buildpath/"src/github.com/atombender/ktail"
     dir.install buildpath.children
 
     cd dir do
-      system "dep ensure -vendor-only"
+      system "glide install --strip-vendor"
       system "make"
       bin.install "build/ktail"
     end
